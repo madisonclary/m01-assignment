8995e8ab47dd61ef8a2862e256077852a9538d74
madisonclary <98995291+madisonclary@users.noreply.github.com>
2023-10-25 19:30:04 -0500

    Add files via upload

---
+++ programming_1.py
@@ -0,0 +1,18 @@
+#1 
+secondsPerMinute = 60
+minutesPerHour = 60  
+
+print(secondsPerMinute*minutesPerHour)
+
+#2
+
+seconds_per_hour = 3600
+
+#3
+print(seconds_per_hour*24)
+
+#4
+seconds_per_day = seconds_per_hour*24
+
+#5
+print(seconds_per_day/seconds_per_hour)
